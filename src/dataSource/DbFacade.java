4ab40f3255ee8a1d9e894e7261931a205d6042bb
Kasper Møller Hald <kasper@17b.dk>
2014-03-26 12:59:45 +0100

    Modifed mapper classes
    Added functionality to apartmentMapper and costumerMapper.

--- src/dataSource/DbFacade.java
+++ src/dataSource/DbFacade.java
@@ -1,42 +1,32 @@
 package dataSource;
 
-
-
-import java.sql.Connection;
-
-
-
-
 /**
  *
  * @author kasper
  */
-
-
-
 import java.sql.Connection;
 
-
 public class DbFacade {
+
     //== Fields
     private UnitOfWorkProcess uow;
     private Connection con;
-    
+
     //== Singleton Start
     private static DbFacade instance;
-    
+
     //== Constructor
-    private DbFacade(){
-        uow = UnitOfWorkProcess.getInstance();
-        con = DBConnector.getInstance().getConnection();
+    private DbFacade() {
+	uow = UnitOfWorkProcess.getInstance();
+	con = DBConnector.getInstance().getConnection();
     }
-    
-    public static DbFacade getInstance(){
-        if(instance == null)
-            instance = new DbFacade();
-        return instance;
+
+    public static DbFacade getInstance() {
+	if (instance == null) {
+	    instance = new DbFacade();
+	}
+	return instance;
     }
 
     //== Singleton End
-    
 }
