3dd250e873953c91776571a6fb28aac1e6b1d7d9
Robert <Robert@10.18.130.9>
2014-04-02 13:14:34 +0200

    Added insertCustomer Method in CustomerMapper
    Reviewed by: Simon

--- src/dataSource/CustomerMapper.java
+++ src/dataSource/CustomerMapper.java
@@ -3,9 +3,7 @@ package dataSource;
 import domain.Customer;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 /**
  *
@@ -14,7 +12,34 @@ import java.util.ArrayList;
 public class CustomerMapper {
 
     public int insertNewCustomer(Customer customer, Connection con) {
-        return 0;
+
+        String insertCustomerSQL = "INSERT INTO CUSTOMER VALUES(customer_seq.NEXTVAL,?,?,?,?,?,?,?,?,0)";
+        int rowsInserted = 0;
+        PreparedStatement statement = null;
+        
+        try {
+            statement = (PreparedStatement) con.prepareStatement(insertCustomerSQL);
+            statement.setString(1, customer.getFirst_name());
+            statement.setString(2, customer.getLast_name());
+            statement.setString(3, customer.getPhone());
+            statement.setString(4, customer.getEmail());
+            statement.setString(5, customer.getCountry());
+            statement.setString(6, customer.getCity());
+            statement.setLong(7, customer.getZipcode());
+            statement.setString(8, customer.getStreet());
+            rowsInserted = statement.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            statement.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return rowsInserted;
     }
 
 }
