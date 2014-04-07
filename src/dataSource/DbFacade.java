d2d5574e996f60423ed6ec50389040526ab99d53
Kasper Møller Hald <kasper@17b.dk>
2014-04-07 13:16:38 +0200

    Controller and facade updated
    New methods added to controller and facade, allowing gui to update
    lists, and retrieve bookings by specific parameters. Reviewed by Kasper.

--- src/dataSource/DbFacade.java
+++ src/dataSource/DbFacade.java
@@ -63,11 +63,37 @@ public class DbFacade {
         return null;
     }
     
-        public ArrayList<Customer> searchForCustomers(String keyword) {
+    public ArrayList<Customer> searchForCustomers(String keyword) {
         if (this.chatty != null) {
             return this.chatty.searchForCustomers(keyword);
         }
         return null;
     }
-
+    
+    public ArrayList<Booking> getBookingsBySpecificDate(String date){
+	if (this.chatty != null) {
+            return this.chatty.getBookingsBySpecificDate(date, con);
+        }
+        return null;
+    }
+    
+    public ArrayList<Booking> getBookingsBySpecificMonth(String month){
+	if (this.chatty != null) {
+            return this.chatty.getBookingsBySpecificMonth(month, con);
+        }
+        return null;
+    }
+    
+    public ArrayList<Booking> getBookingsByApartment(int a_num){
+	if (this.chatty != null) {
+            return this.chatty.getBookingsByApartment(a_num, con);
+        }
+        return null;
+    }
+    
+    public void updateLists(){
+	if (this.chatty != null) {
+            this.chatty.updateLists(con);
+        }
+    }
 }
