74f43257e995440aafd8501d3ca0122ec8fc2a1f
Robert <Robert@10.18.130.75>
2014-04-25 09:41:33 +0200

    90 - added toGuiString in booking
    Reviewed by: Simon

--- src/domain/Booking.java
+++ src/domain/Booking.java
@@ -183,4 +183,21 @@ public class Booking implements Comparable, Cloneable {
         return super.clone();
     }
 
+    public String toGuiListString() {
+
+        return "Booking " + b_id + "/n"
+                + "________________/n"
+                + "Apartment:/t/t" + a_num + "/n"
+                + "Arrival date:/t/t" + date_from + "/n"
+                + "Nights:/t/t" + num_of_nights + "/n"
+                + "Guests:/t/t" + number_of_guests + "/n"
+                + "Agency:/t/t" + travel_agency + "/n"
+                + "Price:/t/t" + price + "/n/n"
+                + "Customer: " + cust_id + "/n"
+                + "________________/n"
+                + "First Name:/t/t" + first_name + "/n"
+                + "Last Name:/t/t" + last_name + "/n"
+                + "Phone: /t/t" + phone;
+
+    }
 }
