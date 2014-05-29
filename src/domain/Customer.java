641cc826d8d4487e9aa96df6478735669a4b77dd
Simon Grønborg <groenborg27@gmail.com>
2014-05-29 11:40:44 +0200

    ToString method for customer changes

--- src/domain/Customer.java
+++ src/domain/Customer.java
@@ -2,9 +2,10 @@ package domain;
 
 /**
  * Contains information about a customer.
+ *
  * @author kasper
  */
-public class Customer implements Cloneable{
+public class Customer implements Cloneable {
 
     private int cust_id;
     private String first_name;
@@ -18,123 +19,116 @@ public class Customer implements Cloneable{
     private int version;
 
     public Customer(int cust_id, String first_name, String last_name, String phone, String email, String country, String city, String zipcode, String street, int version) {
-	this.cust_id = cust_id;
-	this.first_name = first_name;
-	this.last_name = last_name;
-	this.phone = phone;
-	this.email = email;
-	this.country = country;
-	this.city = city;
-	this.zipcode = zipcode;
-	this.street = street;
-	this.version = version;
+        this.cust_id = cust_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone = phone;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.street = street;
+        this.version = version;
     }
 
     public Customer(String first_name, String last_name, String phone, String email, String country, String city, String zipcode, String street) {
-	this.first_name = first_name;
-	this.last_name = last_name;
-	this.phone = phone;
-	this.email = email;
-	this.country = country;
-	this.city = city;
-	this.zipcode = zipcode;
-	this.street = street;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone = phone;
+        this.email = email;
+        this.country = country;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.street = street;
     }
-    
+
     public void setCust_id(int cust_id) {
-	this.cust_id = cust_id;
+        this.cust_id = cust_id;
     }
 
     public void setFirst_name(String first_name) {
-	this.first_name = first_name;
+        this.first_name = first_name;
     }
 
     public void setLast_name(String last_name) {
-	this.last_name = last_name;
+        this.last_name = last_name;
     }
 
     public void setPhone(String phone) {
-	this.phone = phone;
+        this.phone = phone;
     }
 
     public void setEmail(String email) {
-	this.email = email;
+        this.email = email;
     }
 
     public void setCountry(String country) {
-	this.country = country;
+        this.country = country;
     }
 
     public void setCity(String city) {
-	this.city = city;
+        this.city = city;
     }
 
     public void setZipcode(String zipcode) {
-	this.zipcode = zipcode;
+        this.zipcode = zipcode;
     }
 
     public void setStreet(String street) {
-	this.street = street;
+        this.street = street;
     }
 
     public void setVersion(int version) {
-	this.version = version;
+        this.version = version;
     }
 
     public int getCust_id() {
-	return cust_id;
+        return cust_id;
     }
 
     public String getFirst_name() {
-	return first_name;
+        return first_name;
     }
 
     public String getLast_name() {
-	return last_name;
+        return last_name;
     }
 
     public String getPhone() {
-	return phone;
+        return phone;
     }
 
     public String getEmail() {
-	return email;
+        return email;
     }
 
     public String getCountry() {
-	return country;
+        return country;
     }
 
     public String getCity() {
-	return city;
+        return city;
     }
 
     public String getZipcode() {
-	return zipcode;
+        return zipcode;
     }
 
     public String getStreet() {
-	return street;
+        return street;
     }
 
     public int getVersion() {
-	return version;
+        return version;
     }
 
-//    @Override
-//    public String toString() {
-//	return first_name + " " + last_name + " " +phone;
-//    }
-//    
     @Override
     public String toString() {
-        return "Customer{" + "cust_id=" + cust_id + ", first_name=" + first_name + ", last_name=" + last_name + ", phone=" + phone + ", email=" + email + ", country=" + country + ", city=" + city + ", zipcode=" + zipcode + ", street=" + street + ", version=" + version + '}';
+        return first_name + " " + last_name + " " + phone;
     }
-    
-    
-    
+
     @Override
-    public Object clone() throws CloneNotSupportedException{
+    public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
 
